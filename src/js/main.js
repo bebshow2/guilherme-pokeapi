2b7bd3a06ab655fe964040b58702d924301626b1
Guilherme Castro Medeiros <182220011@senacinformatica.br>
2024-10-23 19:46:31 -0300

    adicionado função de buscar

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,26 @@
 import { createCard } from "./card/card.js";
 import { listAllPokemons } from "./fetchApi/fetchfunctions.js";
 
+console.log("carregou!");
 
-console.log("carregou!")
-
-const {count, results, next, previous} = await listAllPokemons();
-
+const { count, results } = await listAllPokemons();
 console.log("pokemons (results): ", results);
 
 results.forEach((pokemon, index) => {
-    console.log("index: ", index);
     createCard(pokemon, index + 1);
+});
+
+const searchInput = document.getElementById('search-input');
+
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const pokemonList = document.getElementById("pokemon-list");
+    
+    pokemonList.innerHTML = '';
+
+    results.forEach((pokemon, index) => {
+        if (pokemon.name.toLowerCase().includes(searchTerm)) {
+            createCard(pokemon, index + 1);
+        }
+    });
 });
