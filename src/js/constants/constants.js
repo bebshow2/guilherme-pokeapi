7d47e9f51f1be093cc95e216effd2ceb738e0afc
Guilherme Castro Medeiros <182220011@senacinformatica.br>
2024-10-29 19:12:37 -0300

    aumentado o limite de cards de pokemons para 151

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,4 +1,3 @@
-export const urlPokeApi = "https://pokeapi.co/api/v2/pokemon";
-
+export const urlPokeApi = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 
 export const pokemonList = document.getElementById("pokemon-list")
