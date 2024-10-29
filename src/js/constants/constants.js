97a7430b8ca59d589d4c07d5780cfc5f6eafea3f
Guilherme Castro Medeiros <182220011@senacinformatica.br>
2024-10-29 20:57:17 -0300

    link anterior do pokeAPI para ajustes

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,3 +1,3 @@
-export const urlPokeApi = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
+export const urlPokeApi = "https://pokeapi.co/api/v2/pokemon";
 
 export const pokemonList = document.getElementById("pokemon-list")
